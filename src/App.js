73059f002743e429eaf886c25daf261d3130e46d
Jitendra Kumar Pal <jpjitendrapal@gmail.com>
2023-05-18 15:39:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Search from "./search/Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello!</h1>
+      <h2>Sample code for search auto complete</h2>
       <Search />
     </div>
   );
